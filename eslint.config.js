a6b808ff6594f630e19ef1a28ca375da7fd0a202
DonAdam2 <adam.morsi@yahoo.com>
2026-03-23 15:23:18 +0200

    remove jest

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,6 @@ const path = require('path'),
   js = require('@eslint/js'),
   react = require('eslint-plugin-react'),
   reactHooks = require('eslint-plugin-react-hooks'),
-  jest = require('eslint-plugin-jest'),
-  testingLibrary = require('eslint-plugin-testing-library'),
-  jestDom = require('eslint-plugin-jest-dom'),
   prettier = require('eslint-plugin-prettier'),
   prettierConfig = require('eslint-config-prettier'),
   typescriptEslint = require('@typescript-eslint/eslint-plugin'),
@@ -41,20 +38,6 @@ module.exports = [
     },
   },
 
-  // Jest test files and setup files
-  {
-    files: ['**/jest/**/*.js', '**/*.test.js', '**/*.spec.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-        ...globals.es2015,
-      },
-    },
-  },
-
   // TypeScript ESLint recommended
   {
     files: ['**/*.{ts,tsx}'],
@@ -78,26 +61,12 @@ module.exports = [
         ...globals.browser,
         ...globals.node,
         ...globals.commonjs,
-        ...globals.jest,
         ...globals.es2015,
-        // Additional Jest globals
-        it: 'readonly',
-        jest: 'readonly',
-        test: 'readonly',
-        expect: 'readonly',
-        describe: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
       },
     },
     plugins: {
       react: react,
       'react-hooks': reactHooks,
-      jest: jest,
-      'testing-library': testingLibrary,
-      'jest-dom': jestDom,
       prettier: prettier,
       '@typescript-eslint': typescriptEslint,
     },
@@ -111,16 +80,6 @@ module.exports = [
 
       // React Hooks recommended
       ...reactHooks.configs.recommended.rules,
-
-      // Jest recommended
-      ...jest.configs.recommended.rules,
-      ...jest.configs.style.rules,
-
-      // Testing Library React
-      ...testingLibrary.configs.react.rules,
-
-      // Jest DOM recommended
-      ...jestDom.configs.recommended.rules,
 
       // Custom overrides from original config
       'prettier/prettier': 'error',
