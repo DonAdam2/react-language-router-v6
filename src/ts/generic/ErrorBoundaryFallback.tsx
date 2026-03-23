8be2369368d21e12c03878bbff609786f138bb38
DonAdam2 <adam.morsi@yahoo.com>
2026-03-23 15:31:29 +0200

    upgrade minor packages

--- src/ts/generic/ErrorBoundaryFallback.tsx
+++ src/ts/generic/ErrorBoundaryFallback.tsx
@@ -3,7 +3,7 @@ import { FallbackProps } from 'react-error-boundary';
 const ErrorBoundaryFallback = ({ error, resetErrorBoundary }: FallbackProps) => (
   <div>
     <h3>Something went wrong:</h3>
-    <pre>{error.message}</pre>
+    <pre>{error instanceof Error ? error.message : String(error)}</pre>
     <button onClick={resetErrorBoundary}>Try Again</button>
   </div>
 );
